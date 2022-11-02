e2ac90e327e2e8ad1267d1fa75926ff868342485
Jeremy Krummenacher <jeremy.krummenacher@icloud.com>
2022-11-02 13:39:08 +0100

    FooBarQix

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -4,9 +4,59 @@ namespace FooBarQix.Tests
 {
     public class FooBarQixDeterminerTest
     {
-        [Fact]
-        public void Test1()
+         [Fact]
+        public void TestForOne()
         {
+            string expected = "1";
+            FooBarQixDeterminer determiner = new FooBarQixDeterminer();
+
+            string actual = determiner.getFoo(1);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestForThree()
+        {
+            string expected = "FooFoo";
+            FooBarQixDeterminer determiner = new FooBarQixDeterminer();
+
+            string actual = determiner.getFoo(3);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestForSix()
+        {
+            string expected = "Foo";
+            FooBarQixDeterminer determiner = new FooBarQixDeterminer();
+
+            string actual = determiner.getFoo(6);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestForTwentyOne()
+        {
+            string expected = "FooQix";
+            FooBarQixDeterminer determiner = new FooBarQixDeterminer();
+
+            string actual = determiner.getFoo(21);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestForOneHundredAndFive()
+        {
+            string expected = "FooBarBarQix";
+            FooBarQixDeterminer determiner = new FooBarQixDeterminer();
+
+            string actual = determiner.getFoo(105);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -3,5 +3,20 @@
     public class FooBarQixDeterminer
     {
 
+        public string getFoo(int number)
+        {
+            string result = "";
+
+            if (number % 3 == 0) { result = "Foo"; }
+            if (number.ToString().Contains("3")) { result += "Foo"; }
+            if (number % 5 == 0) { result += "Bar"; }
+            if (number.ToString().Contains("5")) { result += "Bar"; }
+            if (number % 7 == 0) { result += "Qix"; }
+            if (number.ToString().Contains("7")) { result += "Qix"; }
+
+            if (result == "") { return number.ToString(); }
+            return result;
+        }
+
     }
 }
